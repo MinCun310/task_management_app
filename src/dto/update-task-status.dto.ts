72514605324289597a775bb32a9953c3516560d7
Cuong Duong <minhcuongduong98@gmail.com>
2023-12-02 11:12:00 +0700

    connect to database

--- src/dto/update-task-status.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from "class-validator";
-import { TaskStatus } from "src/models/task-status.enum";
-
-export class UpdateTaskStatusDto {
-    @IsEnum(TaskStatus)
-    status: TaskStatus;
-}
